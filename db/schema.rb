06cdfd25a348d76a2dc054fd95038e1c8e716820
zhangmyihua <michaelzhang2@msn.com>
2015-02-06 17:15:51 -0500

    Creating a user model, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -12,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206192828) do
+ActiveRecord::Schema.define(version: 20150206212904) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150206192828) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
